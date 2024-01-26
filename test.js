4306afcb0f26da0320b3d444fcd69ad96f3ee5c2
Mamun <mamunkhan2002bd@gmail.com>
2024-01-26 16:30:34 +0600

    some worked in js file

--- test.js
+++ test.js
@@ -1,5 +1,25 @@
-let num = 55;
-console.log(isNaN(num));
+let totalTk = 1000;
 
-let num2 = '77';
-console.log(isNaN(num2));
+let apples = 500;
+
+let oranges = 200;
+
+let totalBuy = (apples + oranges);
+
+let back = (totalTk - totalBuy)
+
+console.log(back);
+
+// -----------------
+
+let Mathematics = 75.25;
+let Biology = 65;
+let Chemistry = 80;
+let Physics = 35.45;
+let Bangla = 99.50;
+
+let totalMarks = (Mathematics + Biology + Chemistry + Physics + Bangla);
+
+let averageMarks = (totalMarks / 5);
+
+console.log(averageMarks);
