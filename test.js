d45a0a4e4f8bad103223c834bcd5d27b0c5a061e
Mamun <mamunkhan2002bd@gmail.com>
2024-01-27 11:14:14 +0600

    some working in js

--- test.js
+++ test.js
@@ -52,19 +52,80 @@ Free Drinks
     - Else Coke: 30tk
 */
 
-
-let burgerPrice = 500;
+/*task number one */
+let burgerPrice = 300;
 let cokePrice = 30;
 
 if (burgerPrice >= 500) {
     let coke = 'coke free';
-    console.log('burger price' + ' ' + burgerPrice + ' '  + 'tk');
+    console.log('burger price' + ' ' + burgerPrice + ' ' + 'tk');
     console.log(coke);
 }
 
 else {
     let totalPrice = burgerPrice + cokePrice;
-    console.log( 'coke Price' + ' ' + cokePrice + ' ' + 'tk');
-    console.log('burger price' + ' ' + burgerPrice + ' '  + 'tk');
-    console.log('total price' + ' '  + totalPrice)  + ' ' + 'tk'; 
+    console.log('coke Price' , ' ' , cokePrice,'tk');
+    console.log('burger price' , ' ' , burgerPrice ,'tk');
+    console.log('total price' , ' ' , totalPrice,'tk');
 }
+
+
+
+/*
+BMI Calculator and Health Category
+
+Create a JavaScript program that calculates the Body Mass Index (BMI) and assigns a health category based on the BMI value. Use nested if-else statements to determine the health category.
+
+    - Calculate BMI using the formula: BMI = weight (kg) / (height (m))^2
+    - BMI < 18.5, you are underweight.
+    - BMI >= 18.5 and BMI <=24.9, you are normal.
+    - BMI >=25 and BMI <= 29.9, you are overweight.
+    - Otherwise, you are obese.
+
+ */
+/* task number two */
+/*let BMI;
+let weightBmi = 40;
+let hightBmi = 61;
+
+if (BMI < 18.5) {
+    console.log('You are underweight.');
+} else if (BMI >= 18.5 || BMI >= 24.9) {
+    console.log('you are normal.');
+} else if (BMI >= 25 || BMI <= 29.9) {
+    console.log('you are overweight.');
+} else {
+    console.log('you are obese.');
+}*/
+
+/***
+
+Grade Calculator
+
+Create a simple JavaScript program that takes a student's score as input and returns their corresponding grade based on the following grading scale:
+
+    A: 90-100
+    B: 80-89
+    C: 70-79
+    D: 60-69
+    F: 0-59
+
+***/
+
+// tusk three
+/*
+let result = 99;
+
+if (result >= 90 && result <= 100) {
+    console.log('Grade: A');
+} else if (result >= 80 && result < 90) {
+    console.log('Grade: B');
+} else if (result >= 70 && result < 80) {
+    console.log('Grade: C');
+} else if (result >= 60 && result < 70) {
+    console.log('Grade D');
+} else {
+    console.log('Grade: Fail');
+}
+
+*/
