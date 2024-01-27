b9807aaa194a99956a62673634115c0a87f7fe32
Mamun <mamunkhan2002bd@gmail.com>
2024-01-27 11:40:47 +0600

    some code written in my computer

--- test.js
+++ test.js
@@ -53,21 +53,21 @@ Free Drinks
 */
 
 /*task number one */
-let burgerPrice = 300;
-let cokePrice = 30;
+// let burgerPrice = 300;
+// let cokePrice = 30;
 
-if (burgerPrice >= 500) {
-    let coke = 'coke free';
-    console.log('burger price' + ' ' + burgerPrice + ' ' + 'tk');
-    console.log(coke);
-}
+// if (burgerPrice >= 500) {
+//     let coke = 'coke free';
+//     console.log('burger price' + ' ' + burgerPrice + ' ' + 'tk');
+//     console.log(coke);
+// }
 
-else {
-    let totalPrice = burgerPrice + cokePrice;
-    console.log('coke Price' , ' ' , cokePrice,'tk');
-    console.log('burger price' , ' ' , burgerPrice ,'tk');
-    console.log('total price' , ' ' , totalPrice,'tk');
-}
+// else {
+//     let totalPrice = burgerPrice + cokePrice;
+//     console.log('coke Price' , ' ' , cokePrice,'tk');
+//     console.log('burger price' , ' ' , burgerPrice ,'tk');
+//     console.log('total price' , ' ' , totalPrice,'tk');
+// }
 
 
 
@@ -129,3 +129,36 @@ if (result >= 90 && result <= 100) {
 }
 
 */
+
+
+// ====================================
+
+/***
+
+if you get more then 80 then inside your friend score. 
+    If your friend get more than 80. then go for a lunch. 
+    if your friend get below 80 but greater than or equal 60 then tell your friend, good luck next time. 
+    if your friend get less than 60 but more than or equal to 40 then, keep your friend's message unseen.
+    if your friend get less than 40, block your friend
+if you get less than 80 go to home and sleep and act sad
+
+Note: 
+use nested if-else-if-else
+*/
+
+let myScore = 79;
+let friendScore = 39;
+
+if (myScore >= 80) {
+    if (friendScore > 80) {
+        console.log('Go for a lunch');
+    } else if (friendScore < 80 && friendScore >= 60) {
+        console.log('Good luck next time.');
+    } else if (friendScore < 60 && friendScore >= 40) {
+        console.log('Massage Unseen.');
+    } else {
+        console.log('Block your friend.');
+    }
+} else {
+    console.log('Go to home and sleep and act sad');
+}
