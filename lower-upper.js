d75fc2bdb0e730730271cd3be9872e06d55f6b42
Mamun <mamunkhan2002bd@gmail.com>
2024-01-31 09:44:51 +0600

    some code written

---
+++ lower-upper.js
@@ -0,0 +1,7 @@
+let school = ' bhangura union hight school and collage   ';
+
+let upper = school.toUpperCase()
+
+// console.log(upper.toLowerCase());
+
+console.log(school);
---
+++ slice.js
@@ -0,0 +1,3 @@
+let text = "Apple, Banana, Kiwi";
+let text2 = text.slice(2, 8)
+console.log(text2);
