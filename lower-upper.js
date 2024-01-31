c8537fa4b58346698b3f635c95b8e923fbae2ed9
Mamun <mamunkhan2002bd@gmail.com>
2024-01-31 10:07:11 +0600

    trying to to lowercse or upper case

--- lower-upper.js
+++ lower-upper.js
@@ -4,4 +4,19 @@ let upper = school.toUpperCase()
 
 // console.log(upper.toLowerCase());
 
-console.log(school);
+// console.log(school);
+
+
+let subject = 'English';
+let book = '  english';
+
+if (subject.toUpperCase().trim() === book.toUpperCase().trim()) {
+    // console.log('I am learning all most 6 hours');
+} else {
+    // console.log(`I don't read book`);
+}
+
+
+let text = 'Count how many times a string has the letter';
+
+
--- slice.js
+++ slice.js
@@ -1,3 +1,1 @@
-let text = "Apple, Banana, Kiwi";
-let text2 = text.slice(2, 8)
-console.log(text2);
+
