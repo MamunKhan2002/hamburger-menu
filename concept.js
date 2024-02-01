0f3c3ccb0dc9e0e7ae71eac8043fb5862b95e962
Mamun <mamunkhan2002bd@gmail.com>
2024-02-01 22:34:17 +0600

    Written some code for learning perpose

---
+++ concept.js
@@ -0,0 +1,20 @@
+const points = [1, 3, 4, 21, 32, 45, 23, 56];
+
+// let sum = 0;
+// for (let i = 0; i < points.length; i++) {
+//     sum += points[i];
+//     // console.log(points[i], sum);
+// }
+
+// // console.log(sum);
+
+for (let i = 0; i < points.length; i++) {
+    let point = points[i];
+    if (point % 2 !== 0) {
+        // console.log(point);
+    }
+}
+
+for (let i = 2; i < points.length - 3; i++) {
+    console.log(points[i]);
+}
