a5bd469f284e14b4a1947dfcfd37d0a841a18919
Mamun <mamunkhan2002bd@gmail.com>
2024-02-03 20:46:40 +0600

    Written some code

--- function/avgOddArr.js
+++ function/avgOddArr.js
@@ -1,2 +1,18 @@
-const points = [56, 34, 23, 12, 54, 214, 3];
+const points = [56, 34, 23, 12, 54, 214, 3,];
 
+function avgEvenPoint(value) {
+    const evenPoints = [];
+    let sum = 0;
+    for (let point of points) {
+        if (point % 2 === 0) {
+            evenPoints.push(point)
+            sum += point;
+        }
+    }
+    const len = evenPoints.length;
+    const average = sum / len;
+    return average;
+}
+
+const avgEvenPointResult = avgEvenPoint(points);
+console.log('Avg-Even-Point-Result Is: ', avgEvenPointResult);
