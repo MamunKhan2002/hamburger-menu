e02e7df753536c1eeb35e1775a0e47343bb95591
Mamun <mamunkhan2002bd@gmail.com>
2024-02-24 09:58:49 +0600

    Es 6 practices

---
+++ ES6/map.js
@@ -0,0 +1,36 @@
+
+// //  map  ()
+// const numbers = [1, 3, 5, 7, 9];
+
+// const result = numbers.map(p => p + 1);
+// console.log(result);
+
+
+// // for of loop 
+// const even = [];
+// for (const num of numbers) {
+//     const result = num + 1;
+//     even.push(result);
+// }
+
+// console.log(even);
+
+// const point = [33, 50, 79, 78, 90, 101, 30];
+
+// const pointResult = point.filter(p => p % 10 === 0);
+
+// console.log(pointResult);
+
+// const findResult = point.find(p => p % 10 === 0);
+
+// console.log(findResult);
+
+// const instructor = [
+//     { name: 'Nodi', age: 28, position: 'Senior' },
+//     { name: 'Akil', age: 26, position: 'Junior' },
+//     { name: 'Sobuj', age: 25, position: 'Senior' }
+// ];
+
+// const insResult = instructor.filter(p => p.position === "Senior");
+
+// console.log(insResult);
---
+++ onclickTest/script.js
@@ -0,0 +1,21 @@
+// document.getElementById("text-box").addEventListener("keyup", function (event) {
+//     // console.log(event.target.value);
+//     const boxText = event.target.value;
+// })
+
+// document.getElementById("massage-post").addEventListener("click", function (event) {
+//     const massageContainer = document.getElementById("massage-container");
+//     const textBox = document.getElementById("text-box");
+//     const boxText = event.target.value;
+//     const pera = document.createElement('p');
+//     pera.innerText = boxText;
+//     massageContainer.appendChild(pera);
+
+
+
+// })
+
+document.getElementById("massage-post").addEventListener("click", function (event) {
+    const textBox = document.getElementById("text-box");
+    
+})
